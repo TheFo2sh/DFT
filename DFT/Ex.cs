ceb3a93d40abf447dab327f1a89acaef7b6c157b
Ahmed Fouad <Ahmed.Fouad.net@hotmail.com>
2020-05-17 16:53:35 +0200

    refactor

---
+++ DFT/Ex.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Accord.Math;
+using Accord.Math.Transforms;
+using Data;
+using SkiaSharp;
+
+namespace DFT
+{
+    public static class Ex
+    {
+       
+        public static Complex[] DFT(this IEnumerable<Point> source)
+        {
+            var complex = source.Select(item => new Complex(item.x, item.y)).ToArray();
+            FourierTransform2.DFT(complex, FourierTransform.Direction.Backward);
+            return complex;
+
+        }
+
+
+     
+        public static SKPoint ToPoint(this IEnumerable<Complex> drawingData,
+            double time, double x, double y)
+        {
+            for (int i = 0; i < drawingData.Count(); i++)
+            {
+                var item = drawingData.ElementAt(i);
+                x += item.Magnitude * Math.Cos(i * time + item.Phase);
+                y += item.Magnitude * Math.Sin(i * time +  item.Phase);
+            }
+
+            return new SKPoint((float)x,(float)y);
+
+
+        }
+    }
+}
--- DFT/MainPage.xaml.cs
+++ DFT/MainPage.xaml.cs
@@ -6,8 +6,6 @@ using System.Numerics;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Accord.Math;
-using Accord.Math.Transforms;
 using Data;
 using SkiaSharp;
 using SkiaSharp.Views.Forms;
@@ -17,52 +15,6 @@ using Point = Data.Point;
 
 namespace DFT
 {
-    public static class Ex
-    {
-        public static Complex[] DFT(this double[] source)
-        {
-            var complexes = source.Select(x =>  new Complex(x, 0)).ToArray();
-            FourierTransform2.DFT(complexes, FourierTransform.Direction.Backward);
-            return complexes;
-
-        }
-        public static Complex[][] DFT(this double[][] source)
-        {
-            List<Complex[]> res = new List<Complex[]>();
-            var val = source.Select(item => item[0]).ToArray().DFT();
-            var val2 = source.Select(item => item[1]).ToArray().DFT();
-            for (int i = 0; i < val.Count(); i++)
-            {
-                res.Add(new Complex[] { val[i], val2[i] });
-            }
-
-            return res.ToArray();
-            var complexes=source.Select(item =>item.Select(x=> new Complex(x, 0)).ToArray()).ToArray();
-            FourierTransform2.DFT2(complexes,FourierTransform.Direction.Backward);
-            
-            return complexes;
-            
-        }
-
-
-     
-        public static SKPoint ToPoint(this List<Complex[]> drawingData,
-            double time, double x, double y)
-        {
-            for (int i = 0; i < drawingData.Count(); i++)
-            {
-                var item = drawingData[i];
-                x += item[0].Magnitude * Math.Cos(i * time + 0 + item[0].Phase);
-                y += item[1].Magnitude * Math.Sin(i * time + Math.PI/2 + item[1].Phase);
-            }
-
-            return new SKPoint((float) x, (float) y);
-
-
-        }
-    }
-
-   
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
@@ -79,31 +31,31 @@ namespace DFT
 
         public void Setup()
         {
-            var points = Drawing.TwitterImagePoints;
+            var points = Drawing.FactoryImagePoints;
             Count = points.Length;
-            var items = points.Select(item => new[] {item.x, item.y}).ToArray();
-            _complex = items.DFT().ToList();
+            _complex = points.DFT().ToArray();
 
 
           
         }
 
         public int Count { get; set; }
-        private List<Complex[]> _complex;
+        private Complex[] _complex;
 
         private  async void SKCanvasView_OnPaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
             var canvas = e.Surface.Canvas;
-
-            var point= _complex.ToPoint(time,500,500);
+            canvas.Clear(SKColors.Black);
+            var point= _complex.ToPoint(time,this.Width/2+100,this.Height/2);
 
             _path.Insert(0, point);
 
             canvas.DrawPoints(SKPointMode.Polygon, _path.ToArray(), new SKPaint()
             {
-                Color = SKColors.Black,
+                Color = SKColors.White,
                 StrokeWidth = 5,
-                StrokeJoin = SKStrokeJoin.Round,
+                
+            StrokeJoin = SKStrokeJoin.Round,
                 FilterQuality = SKFilterQuality.High,
                 StrokeCap = SKStrokeCap.Round,
                 IsAntialias = true,
